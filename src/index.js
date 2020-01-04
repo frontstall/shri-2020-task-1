3344620f71d0d220139cd4567468c12d59d29ffa
frontstall <deepspiral@yandex.ru>
2020-01-04 21:04:57 +0300

    add accordion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './scss/index.scss';
+
+const accordionsButtons = Array.from(
+  document.querySelectorAll('.e-accordion__short'),
+);
+
+const accordionsHiddenParts = document.querySelectorAll('.e-accordion__more');
+
+accordionsHiddenParts.forEach((button) => {
+  button.classList.add('visually-hidden');
+});
+
+document.addEventListener('click', (evt) => {
+  const { target } = evt;
+
+  const accordionButton = accordionsButtons.find((button) =>
+    button.contains(target),
+  );
+
+  if (!accordionButton) return;
+
+  const accordionHiddenPart = accordionButton.parentNode.querySelector(
+    '.e-accordion__more',
+  );
+
+  accordionHiddenPart.classList.toggle('visually-hidden');
+});
